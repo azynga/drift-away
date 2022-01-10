c060de85aa0f7c18a795b7b9e5aa73c39ced8fc1
Aljoscha Zynga <aljoscha.zynga@gmail.com>
2022-01-10 13:43:13 +0100

    change gravity calculation

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
+const fps = 60;
 
 class Scene {
     constructor() {
@@ -15,8 +15,28 @@ class Scene {
 }
 
 class Orb {
+    constructor(position, size) {
+        this.position = position;
+        this.size = size;
+        this.acceleration = {
+            x: 0,
+            y: 0
+        };
+        this.speed = {
+            x: 0,
+            y: 0
+        }
+        this.gravity = this.size ** 2 * Math.PI;
+    }
+
+    getGravity() {
+        return this.gravity;
+    }
+}
+
+class Star extends Orb {
     constructor() {
-        
+        super();
     }
 }
 
@@ -34,53 +54,55 @@ class Player {
             x: this.starPosition.x,
             y: this.starPosition.y + 100
         }
-        this.trailPixels = [];
+        this.trailPixels = [{...this.position}];
         this.acceleration = {
             x: 0,
             y: 0
         };
         this.speed = {
             x: 1,
-            y: -0.1
+            y: 0
         };
+        this.direction = this.getDirection();
         this.abilityActive = false;
     }
 
-    posRelation() {
+    getDirection() {
+        // const coordinateDiff = {
+        //     x: this.position.x - this.trailPixels[1].x,
+        //     y: this.position.y - this.trailPixels[1].y
+        // };
+        
+        const angle = Math.atan2(this.speed.y, this.speed.x);
+        console.log(angle)
+        return angle;
+    }
+
+    getRelation() {
         const coordinateDiff = {
             x: this.starPosition.x - this.position.x,
             y: this.starPosition.y - this.position.y
         };
     
         const distance = Math.sqrt(coordinateDiff.x ** 2 + coordinateDiff.y ** 2);
-        if(distance < this.starRadius + this.radius) {
-            this.speed.x = -this.speed.y;
-            this.speed.y = -this.speed.x;
-        }
         const angle = Math.atan2(coordinateDiff.y, coordinateDiff.x);
-
-        return {
+        const relation = {
             distance: distance,
             angle: angle
         }
+        return relation;
     }
 
     setGravity() {
-        const angle = this.posRelation().angle;
-        const distance = this.posRelation().distance;
+        const angle = this.getRelation().angle;
+        const distance = this.getRelation().distance;
+        const GRAVITATIONAL_CONSTANT = 6.67e-11;
         const gravity = {
-            x: (Math.cos(angle) * this.starGravity) / distance ** 2,
-            y: (Math.sin(angle) * this.starGravity) / distance ** 2
+            x: (Math.cos(angle) * this.starGravity) * 0.004 * (this.radius ** 2 * Math.PI) / distance ** 2,
+            y: (Math.sin(angle) * this.starGravity) * 0.004 * (this.radius ** 2 * Math.PI) / distance ** 2
         };
-
         this.acceleration.x = gravity.x;
         this.acceleration.y = gravity.y;
-
-        // console.log('Angle: '+angle);
-        // console.log('AccX: '+gravity.x);
-        // console.log('SpeedX: '+this.speed.x);
-        // console.log('AccY: '+gravity.y);
-        // console.log('SpeedY:'+this.speed.y);
     }
 
     applyAcceleration() {
@@ -97,20 +119,36 @@ class Player {
         ctx.stroke();
     }
 
-    setNextCoordinates() {
-        this.applyAcceleration();
-        this.speed.x += this.acceleration.x / 100;
-        this.speed.y += this.acceleration.y / 100;
-        this.position.x += this.speed.x;
-        this.position.y += this.speed.y;
-        trail.unshift({...player.position});
+    collision() {
+        const distance = this.getRelation().distance;
+        if(distance < this.starRadius + this.radius) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
+    setNextCoordinates() {
+        this.applyAcceleration();
+        if(this.collision()) {
+            this.speed = { x: 0, y: 0};
+        } else {
+            this.speed.x += this.acceleration.x / 100;
+            this.speed.y += this.acceleration.y / 100;
+        }
+        this.position.x += this.speed.x;
+        this.position.y += this.speed.y;
+
+        player.trailPixels.unshift({...player.position});
+        if(player.trailPixels.length > 1200) {
+            player.trailPixels.pop();
+        };
+    }
     
     draw() {
         const drawTrail = () => {
-            trail.forEach((position, index) => {
-                const opacity = 0.4 - (index / (trail.length * 2));
+            player.trailPixels.forEach((position, index) => {
+                const opacity = 0.4 - (index / (player.trailPixels.length * 2));
                 const size = 2;
                 ctx.fillStyle = `rgba(200, 160, 230, ${opacity})`;
                 ctx.fillRect(position.x-size/2, position.y-size/2, size, size);
@@ -138,29 +176,17 @@ class Player {
 
 const scene = new Scene();
 const player = new Player();
-const trail = [];
-const fps = 60;
 
 const update = () => {
+    player.getDirection();
     scene.clear();
-    
-    
-    if(trail.length > 1200) {
-        trail.pop();
-    }
-    // console.log(player.trailPixels);
     player.draw();
-    
     setTimeout(() => {
         requestAnimationFrame(update);
-
     }, 1000 / fps)
 }
 
 update();
-// setInterval(() => {
-//     console.log(trail)
-// }, 1000)
 
 document.addEventListener('keydown', (event) => {
     if(!player.abilityActive){
